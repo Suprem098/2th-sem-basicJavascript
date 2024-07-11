212e28f2eb5dba64f0fc633c4a4a0a781c1d4ead
Suprem098 <supremegiri505@gmail.com>
2024-07-11 08:03:23 +0545

    scopping

---
+++ index.js
@@ -0,0 +1,572 @@
+//  variables
+// var x=90;
+// console.log(x);
+// var x=500;
+// console.log(x);
+
+// let y=90;
+// console.log(y);
+//  y=500;
+// console.log(y);
+
+// const result=9+9
+// console.log(result);
+// const result=6-3;
+// comsole.log(result);
+
+// data types
+// let x=90;
+// console.log(x);
+// console.log(typeof x);
+
+// string
+// const result= "suprem giri";
+// console.log(result);
+// console.log(typeof result);
+
+// undefined
+// let a;
+// console.log(a);
+// console.log(typeof a);
+
+// null
+// const y=null;
+// console.log(y)
+// console.log(typeof y);
+
+// boolean
+// const istrue=true;
+// console.log(istrue);
+// console.log(typeof istrue)
+
+// float
+// const result=5.78;
+// console.log(result);
+// console.log(typeof result);
+
+// string
+//  const output="suprem giri"
+//  console.log(output);
+//  console.log(typeof output);
+
+// boolean
+
+// const ismarried =false;
+// console.log(ismarried);
+// console.log(typeof ismarried);
+
+//  undefined
+// let a;
+// console.log(a);
+// console.log(typeof a);
+
+// null
+
+// const x= null;
+// console.log(x);
+// console.log(typeof x);
+// \bigInt
+// const x= 754065108n;
+// console.log(x);
+// console.log(typeof x);
+
+// symbol
+//  const result = Symbol ("who are you");
+//     console.log(result);
+//     console.log(typeof result);
+
+// Object type
+
+// const obj ={
+//     fname:"suprem giri",
+//     age:19,
+//     Ismarried:false,
+//     occupation:"developer",
+//     salary:45000,
+// };
+// console.log(obj);
+// console.log(typeof obj);
+
+// const arr =[3,4,5,6,7];
+// console.log(arr);
+// console.log(typeof arr); 
+
+// const date = new Date();
+// console.log(date);
+// console.log(typeof date);
+
+
+
+// type conversion
+
+// const x = "123";
+// const val = Number(x);
+// console.log(val);
+// console.log(typeof val);
+
+
+// const result = "8.98";
+// const output = parseFloat(result);
+// console.log(output);
+// console.log(typeof output);
+
+// const result= 1+"1";
+// console.log(result);
+// console.log(typeof result); 
+
+// const result= 8-"1";
+// console.log(result);
+// console.log(typeof result); 
+
+// const x=123;
+// const result = x.toString();
+// console.log(result);
+// console.log(typeof result);
+
+// const x=123;
+// const result = String(x);
+// console.log(result);
+// console.log(typeof result);
+
+// let x=2;
+// let y=true;
+// console.log(true+x);
+// let x=3;
+// let y=false;
+// console.log(x+false);
+// let x="";
+// let result=Boolean(x);
+// console.log(result);
+// console.log(typeof result);
+
+// let result=true;
+// let output=String(result);
+// console.log(output);
+// console.log(typeof output);
+// let date= new Date();
+// let result= date.toString();
+// console.log(result);
+// console.log(typeof result);
+
+// data type
+
+// String Method
+// const result="Hello World";
+// // length
+// console.log(result.length)
+// // charAt
+// console.log(result.charAt(2))
+
+
+// // indexof
+// console.log(result.indexOf("o"))
+// // lastindexof
+// console.log(result.lastIndexOf("o"))
+
+// // lowercase
+// console.log(result.toLowerCase());
+//     // uppercase
+//     console.log(result.toUpperCase());
+//     // split
+//     console.log(result.split(" "));
+//     // includes
+//     console.log(result.includes("World"));
+//     // replace
+//     console.log(result.replace("World","Everyone"));
+//     // slice
+//     console.log(result.slice(1,6));
+//     // substring
+//     console.log(result.substring(1,6));
+//     // trim
+//     const output="        haina k ho yesto yo.Chakka parxu";
+//     console.log(output.trim());
+//     // concatination
+//     console.log(result.concat(" I am JavaScript"))
+
+// Operators
+// assignment operators
+// let y= 90;
+// y += 120;
+// console.log(y);
+// comparison operators
+// let a=10;
+// let b=5;
+// let c="10"
+// // 
+//  console.log(a==b);
+//  console.log(a===b);
+//  console.log(a != b);
+//  console.log(a !== c);
+//  console.log(a>b);
+//  console.log(a<b);
+//  console.log(a>=10);
+//  console.log(b <= 5);
+ // logical operators
+
+//  let x=true;
+//  let y=false;
+//  console.log(x && y);
+//  console.log(x || y);
+//  console.log(!x);
+// const str1 ="Hello";
+// const str2 ="World";
+// console.log(str1  + " " + str2);
+// termany opertor
+
+// let ageVote=18;
+// let result= ageVote >=18? "You can vote": "You can't vote";
+// console.log(result);
+// 
+// const result="Hello World";
+// const output ="I Love JavaScript";
+// const  str3 =" sbvuwvui82frpe2fbhfv";
+// console.log(`${result} ${output} ${str3}` );
+
+// const a=98;
+// const b=29;
+// const c=67;
+// const ans = a+b+c;
+// console.log(`The sum of three number is: ${ans}`);
+
+// Unary operations
+// let ans="234";
+// let output =-ans;
+// console.log(output);
+// console.log(typeof output);
+// let ans="234";
+// let output =+ans;
+// console.log(output);
+// console.log(typeof output);
+// const obj ={
+//     name:"Ram thapa",
+//     age :24
+// }
+// delete obj.age;
+// console.log(obj);
+
+// If else
+// let  x=85;
+// if(x>=90){
+//     console.log("Grade A+");
+    
+// }
+// else if(x>=80){
+//     console.log("Grade A");
+// }
+// else if(x>=70){
+//     console.log("Grade B+");
+// }
+// else if(x>=60){
+//     console.log("Grade B");
+// }
+// else if(x>=50){
+//     console.log("Grade C+");
+// }
+// else if(x>=40){
+//     console.log("Grade C");
+// }
+// else{
+//     console.log("Fail");
+// }
+
+// let email="ramthapa987@gmail.com";
+// let password="123456789";
+// if(email === "ramthapa987@gamil.com" && password === "123456789"){
+//     console.log("Login Successfull");
+// } else {
+//     console.log("invaild password");
+// }
+// let weather ="rainy";
+// if(weather === "sunny"){
+//     console.log("gham lagla");
+// } else if(weather === "winter"){
+//     console.log("gham nalagla");
+// }
+// else if(weather === "cloudly"){
+//     console.log("badal dhakka makka xa");
+// }
+// else if(weather==="rainy"){
+//     console.log("chhata bokna parla");
+// }
+// else {
+//     console.log("invalid weather");
+// }
+// let x=10;
+// if(x>=5){
+//     console.log("x is greater or equal to 5");
+// }
+// else{
+//     console.log("x is less than 5");
+// }
+// switch (control statement)
+
+// let role ="super admin";
+// switch (role) {
+//     case "seller":
+//         console.log("seller can add product")
+        
+//         break;
+//         case "buyer":
+//         console.log("buyer can purchase product")
+        
+//         break;
+
+//         case "admin":
+//             console.log("admin can add seller")
+//             break;
+
+//             case "super admin":
+//         console.log("can control eveything")
+//         break;
+        
+//         break;
+//     default:
+//         console.log("invalid role");
+//         break;
+// }
+// let shipping="delivered";
+// switch (shipping) {
+//     case "processing":           
+//         console.log("your order is processing");
+//         break;
+//         case "shipped":
+//         console.log("your order is shipped");
+//         break;
+//         case "delivered":
+//         console.log("your order is delivered");
+//         break;
+//         default:
+//             console.log("invalid shipping status");
+//             break;
+//             }
+// status code
+//  let StatusCode=200;
+//  switch (StatusCode) {
+//     case 201:
+//         console.log("created");
+        
+//         break;
+//  case 400:
+//      console.log("bad request");
+//      break
+//      case 401:
+//          console.log("unauthorized");
+//          break;
+//          case 403:
+//              console.log("forbidden");
+//              break;
+//              case 500:
+//                  console.log("internal server error!");
+//                  break;
+//                  case 200:
+//                      console.log("ok");
+//    break;
+//      default:
+//       console.log("invalid status code");
+//          break;
+
+//         break;
+//  }
+
+// console.log("I Love JavaScript");
+// console.log("I Love JavaScript");
+// console.log("I Love JavaScript");
+// console.log("I Love JavaScript");
+// console.log("I Love JavaScript");
+// console.log("I Love JavaScript");
+// console.log("I Love JavaScript");
+// console.log("I Love JavaScript");
+// console.log("I Love JavaScript");
+// console.log("I Love JavaScript");
+// Loop
+// for loop
+// for (let i = 1; i<=10; i++){
+//     console.log(`I Love JavaScript: ${i}`);
+// }
+// for (let i=1; i<10; i++){
+//     console.log(`3*${i}=${3*i}`);
+// }
+// even number
+// for (let i=1; i<20;i++){
+//     if (i%2==0){
+//         console.log(`even number: ${i}`);
+// }
+// }
+// for (let i=1; i<20;i++){
+//     if (i%2 !==0){
+//         console.log(`odd number: ${i}`);
+// }
+// }
+// while loop
+
+// let i=1;
+// while (i<=10) {
+    
+//     console.log("I Love JavaScript" +i);
+//     i++;
+// }
+// do while
+// let i=1;
+// do{
+//     console.log("Hello World" +i);
+
+// }while(i<=10);
+// array
+// let result =[3,4,5,6,7,8];
+// let sum =0;
+// for (let i=0; i<result.length;i++){
+//     sum+= result[i];
+
+// }
+// console.log(`The sum of arr is:${sum}`);
+// array method
+//  let arr=[4,5,6,7,8,2,3,4];
+// length
+// console.log(arr.length);
+// PUSH
+
+// arr.push(12);
+// console.log(arr);
+
+
+// pop
+// arr.pop();
+// console.log(arr);
+// shift
+
+// arr.shift();
+// console.log(arr);
+// unshift
+//  arr.unshift(12);
+//  console.log(arr);
+//  splice
+// arr.splice(1,3);
+// console.log(arr);
+
+// let result=["JavaScript","is","Funny","LAnguage"]
+// console.log(result.join(" "))
+
+// slice
+// let result =arr.slice(1,4);
+// console.log(result);
+// includes
+// let result =arr.includes(4);
+// console.log(result);
+// indexOF
+// let result =arr.indexOf(4);
+// console.log(result);
+// let result =arr.lastIndexOf(4);
+// console.log(result);
+// concat
+// console.log(arr.concat("Hello"));
+
+// let arr= [];
+// let sum =0;
+// for (let i=1; i<=10; i++){
+//     arr.push(i);
+// }
+// console.log(arr);
+
+// for(let i=0;i<arr.length;i++){
+//     sum += arr[i];
+// } 
+// let result=sum*2;
+// console.log(`The sum of arr is :${sum}`);
+// console.log(`The multiplication of the number is :${result}`);
+// Object
+// const person={
+//     name:"Ram Thapa",
+//     age:20,
+//     occupation:"programmer",
+//     isMarried:false,
+//     address:{
+//         street:"bafal",
+//         city:"kathmandu",
+//         country:"Nepal",
+//     },
+//     greet:function(){
+//         console.log(`hello my name is:${this.name}and i am :${this.age}years old`);
+//     },
+    
+// };
+
+// // Destructure
+// const{
+//     name,
+//     age,
+//     occupation,
+//     isMarried,
+//     address: {street,city,country},
+
+// }= person;
+// console.log(name,age,occupation,isMarried,street,country,city);
+// person.greet();
+
+
+// function
+//  function myFunc(a,b) {
+//     return a+b;
+//  }
+// let result= myFunc(8,9);
+// console.log(result);
+// function Circle(radius){
+//     return Math.PI * radius * radius;
+
+// }
+// let result=Circle(5)
+// console.log(`The area of circle is :${result}`);
+// function Greet(name,age,education){
+//     console.log(`Hello my name is:${name}.I am :${age} year old and I have completrd my bachelors in :${education} from Bernhardt College of IT`);
+
+// }
+// Greet("Suprem  Giri", 24,"BCA");
+
+// JavaScript have 3 types ways to print message
+// console.log
+// alert
+// prompt
+
+// function myFunc(){
+//     let a= prompt("Enter your number");
+//     let b= prompt("Enter your number");
+//     let result= Number(a)+Number(b);
+//     if(result){
+//         alert(`The sum of number is :${result}`);
+
+//     }
+// }
+// myFunc();
+
+// calculatingCartValue
+// function CalculateTotal(cartItems,taxrate){
+//     let total=0;
+//     for(let i=0;i<cartItems.length;i++){
+//         total+=cartItems[i].price * cartItems[i].quantity
+//     }
+//     total+= total*taxrate;
+//     return total;
+// }
+// let cart=[
+//     {name:"Dr Martin",price:8000,quantity:1},
+//     {name:"AirForce",price:8500,quantity:2}
+// ]
+// let ans= CalculateTotal(cart,0.02);
+// console.log(`your full and final payment is:${ans}`);
+
+// scoping;
+
+// function myFunc(){
+//     if(true){
+// var x=100;
+// let z= 80;
+// const y=20;
+
+//    console.log(y);
+// }
+// console.log(x);
+   
+// }
+
+// myFunc();
+
+
